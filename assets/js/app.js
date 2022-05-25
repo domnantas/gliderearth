917a69b5ac22b508c43294de425e968b659d4fe3
Domantas Petrauskas <domantas@zyro.com>
2022-05-25 22:46:16 +0300

    Add Cesium

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import * as Cesium from "cesium";
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
@@ -43,3 +44,4 @@ liveSocket.connect()
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
 
+console.log(Cesium);
